0102f3d98eef4fb73762f0d0df2c82211c1dbf48
Vic Wen <vic20050418@gmail.com>
2023-09-25 09:51:49 +0800

    update week 2

---
+++ week 2/C1120918Q01/main.c
@@ -0,0 +1,13 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float a, b;
+	scanf("%f %f", &a, &b);
+	float c = sqrt(pow(a, 2) + pow(b, 2));
+	printf("%.2f\n", c);
+	printf("%.2f %.2f", a/c, b/c);
+	return 0;
+}
