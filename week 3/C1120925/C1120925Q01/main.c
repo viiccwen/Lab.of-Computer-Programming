2181a1cb6841f75bfcc85a9ecb788a0fdb32b63a
Vic Wen <vic20050418@gmail.com>
2023-09-25 09:37:47 +0800

    update week 3

---
+++ week 3/C1120925/C1120925Q01/main.c
@@ -0,0 +1,15 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main() {
+	float a = 0, b = 0, c = 0;
+	char op;
+	scanf("%f%c%f", &a, &op, &b);
+	if (op == '+') c = a + b;
+	else if (op == '-') c = a - b;
+	else if (op == '*') c = a * b;
+	else if (op == '/') c = a / b;
+	printf("%.2f%c%.2f=%.2f", a, op, b, c);
+	return 0;
+}
---
+++ week 3/C1120925/C1120925Q02/main.c
@@ -0,0 +1,15 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N - i; j++) printf(" ");
+		for (int k = 1; k <= i * 2 - 1; k++) printf("%c", 64 + i);
+		printf("\n");
+	}
+	return 0;
+}
