7d1beaa3072c9b754f1ba7486fa7752ada7ea4f9
Vic Wen <vic20050418@gmail.com>
2023-09-25 09:45:35 +0800

    update week 1

---
+++ week 1/C1120911/C1120911Q02/main.c
@@ -0,0 +1,14 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	float a = 0, b = 0, c = 0;
+	scanf("%f%f", &a, &b);
+	printf("%.2f + %.2f = %.2f\n", a, b, a + b);
+	printf("%.2f - %.2f = %.2f\n", a, b, a - b);
+	printf("%.2f * %.2f = %.2f\n", a, b, a * b);
+	printf("%.2f / %.2f = %.2f\n", a, b, a / b);
+	return 0;
+}
---
+++ week 1/C1120911/C1120911Q03/main.c
@@ -0,0 +1,11 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	char* a[50];
+	gets(a);
+	puts(a);
+	return 0;
+}
