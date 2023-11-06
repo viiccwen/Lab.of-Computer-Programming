561a534cb1feb45714a8e79922170f2275159429
Vic Wen <vic20050418@gmail.com>
2023-11-06 10:23:42 +0800

    update week 8

---
+++ week 8/C1121106/C1121106Q01/main.c
@@ -0,0 +1,47 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted. The algorithm, which is a comparison sort, is named for the way smaller or larger elements "bubble" to the top of the list.
+Now, with given several integer numbers, write a program to rearrange them in ascending order by using bubble sort.
+
+The first line of input is an integer N (N <= 100) that indicates the size of the sequence. Then, the next line will be followed by a sequence of integers separated by spaces.
+The output should show the sorted integer sequence.
+
+input:
+5
+22 37 19 11 38
+
+output:
+11 19 22 37 38
+*/
+
+void Swap(int* a, int* b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+
+	int arr[101];
+	for (int i = 0; i < N; i++) 
+		scanf("%d", &arr[i]);
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (arr[i] > arr[j]) Swap(&arr[i], &arr[j]);
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		printf("%d", arr[i]);
+		if (i != N - 1) printf(" ");
+	}
+	return 0;
+}
---
+++ week 8/C1121106/C1121106Q02/main.c
@@ -0,0 +1,66 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+/*
+Write a program to read a sequence of integer numbers and found out the repeated numbers and show the result sorted in ascending order.
+The first line of input is an integer N (N <= 100) that indicates the size of the sequence. Then, the next line will be followed by a sequence of integers separated by spaces.
+The output should show the repeated integer numbers in ascending order. If no repeated numbers were found, show message 'No repeated numbers!' instead.
+
+input:
+10
+10 15 23 15 55 78 55 15 55 1112
+
+output:
+15 55
+*/
+
+void Swap(int* a, int* b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void solve() {
+	int N;
+	int arr[101] = { 0 };
+	int repeat[101] = { 0 };
+	int ite = -1;
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++)
+			if (arr[i] > arr[j]) Swap(&arr[i], &arr[j]);
+	}
+
+	int pre = arr[0];
+	for (int i = 1; i < N; i++) {
+		if (pre == arr[i] && ite == -1) {
+			ite = 0;
+			repeat[ite] = arr[i];
+		}
+		else if (pre == arr[i] && ite != -1 && pre != repeat[ite]) {
+			ite++;
+			repeat[ite] = arr[i];
+		}
+		else if (pre != arr[i]) pre = arr[i];
+	}
+
+	if (ite == -1) {
+		printf("No repeated numbers!");
+		return;
+	}
+
+	for (int i = 0; i <= ite; i++) {
+		printf("%d", repeat[i]);
+		if (i != ite) printf(" ");
+	}
+}
+
+int main(){
+	solve();
+	return 0;
+}
