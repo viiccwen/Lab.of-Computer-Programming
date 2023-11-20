60cd79e507a61470d1c45bbac39a72dff5b91b2c
Vic Wen <vic20050418@gmail.com>
2023-11-20 21:47:26 +0800

    update week 9

---
+++ week 9/C1121120/C1121120Q01/main.c
@@ -0,0 +1,127 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+char maze[15][15];
+int start[2];
+
+int pos[2];
+int dir = 0;
+int step[4][2] = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+
+// index->  0       1     2      3
+// dir  -> east , south, west, north
+// step -> {0,1}, {1,0}, {0,-1}, {-1,0}
+
+bool ThereHaveTwoFxckingHoles() {
+	int exist = 0;
+	for (int i = 0; i < 15; i++) 
+		if (maze[i][0] == 'X') exist++;
+		
+	for (int i = 0; i < 15; i++) 
+		if (maze[0][i] == 'X') exist++;
+	
+	for (int i = 0; i < 15; i++) 
+		if (maze[14][i] == 'X') exist++;
+	
+	for (int i = 0; i < 15; i++) 
+		if (maze[i][14] == 'X') exist++;
+	
+	if (exist == 1) return false;
+	else return true;
+}
+
+bool Game() {
+	 do {
+		if (maze[pos[0] + step[dir + 1 <= 3 ? dir + 1 : 0][0]][pos[1] + step[dir + 1 <= 3 ? dir + 1 : 0][1]] == 'X') {
+			dir = (dir + 1 <= 3) ? dir + 1 : 0;
+			pos[0] += step[dir][0];
+			pos[1] += step[dir][1];
+		}
+		else if (maze[pos[0] + step[dir][0]][pos[1] + step[dir][1]] == 'X') {
+			pos[0] += step[dir][0];
+			pos[1] += step[dir][1];
+		}
+		else if (maze[pos[0] + step[dir - 1 == -1 ? 3 : dir - 1][0]][pos[1] + step[dir - 1 == -1 ? 3 : dir - 1][1]] == 'X') {
+			dir = (dir - 1 == -1) ? 3 : dir - 1;
+			pos[0] += step[dir][0];
+			pos[1] += step[dir][1];
+		}
+		else {
+			if (dir % 2 == 0) dir = 2 - dir;
+			else dir = 4 - dir;
+			pos[0] += step[dir][0];
+			pos[1] += step[dir][1];
+		}
+
+		if (start[0] == pos[0] && start[1] == pos[1]) return false;
+
+	 } while (pos[0] != 0 && pos[0] != 14 && pos[1] != 0 && pos[1] != 14);
+
+	return true;
+}
+
+bool isSolution() {
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j< 15; j++) {
+			scanf("%c", &maze[i][j]);
+		}
+		if(i != 14) scanf("\n");
+	}
+
+	// if (!ThereHaveTwoFxckingHoles()) return false;  這是bug 判斷兩個洞就會過了 = =
+	// else return true;
+
+	for (int i = 0; i < 15; i++) {
+		if (maze[i][0] == 'X') {
+			start[0] = pos[0] = i;
+			start[1] = pos[1] = 0;
+		}
+	}
+
+	return Game();
+}
+
+int main()
+{
+	if (isSolution()) printf("This maze has a solution");
+	else printf("This maze has no solution");
+	return 0;
+}
+
+/*
+###############
+#XXXXXXXXX#XXX#
+#X#X#####X###X#
+#X#X#XXXXXXX#X#
+#X#######X###X#
+#X#X#XXXXXXXXX#
+###X###X#######
+#XXXXXXX#X#XXX#
+#X#######X#X###
+XX#XXXXX#XXX#XX
+#X#X###X#X#X#X#
+#XXX#XXXXX#XXX#
+#X#####X#####X#
+#XXXXX#XXXXX#X#
+###############
+
+###############
+#X#XXXXXXXXX#X#
+#X#X#X###X#X#X#
+#XXX#X#XXX#XXX#
+#########X###X#
+XXXXXXXXXXXX#X#
+###X#######X###
+#XXXXXXXXX#XXX#
+#X#X#####X###X#
+#X#X#XXXXXXX#X#
+#X#######X###X#
+#X#X#XXXXXXXXX#
+###X###X#######
+#XXX###XXXXXXX#
+###############
+
+*/
---
+++ week 9/C1121120/C1121120Q02/main.c
@@ -0,0 +1,92 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+/*
+Have you ever played Minesweeper? It’s a cute little game which comes within a certain operating system which name we can’t really remember. Well, the goal of the game is to find where are all the mines within a M × N field. To help you, the game shows a number in a square which tells you how many mines there are adjacent to that square. For instance, suppose the following 4 × 4 field with 2 mines (which are represented by an '*' character):
+
+The input will consist of an arbitrary number of fields. The first line of each field contains two integers n and m (0 < n, m ≤ 100) which stands for the number of lines and columns of the field respectively. The next n lines contains exactly m characters and represent the field.
+Each safe square is represented by an '.' character (without the quotes) and each mine square is represented by an '*' character (also without the quotes). The first field line where n = m = 0 represents the end of input and should not be processed.
+
+For each field, you must print the following message in a line alone:
+Field #x:
+Where x stands for the number of the field (starting from 1). The next n lines should contain the field with the '.' characters replaced by the number of adjacent mines to that square. There must be an empty line between field outputs.
+
+input:
+4 4
+*...
+....
+.*..
+....
+3 5
+**...
+.....
+.*...
+0 0
+
+output:
+Field #1:
+*100
+2210
+1*10
+1110
+
+Field #2:
+**100
+33200
+1*100
+
+*/
+
+
+int m, n;
+int number = 1;
+char mat[101][101];
+char ans[101][101];
+int dir[8][2] = { {-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1} };
+
+// * bomb  . space
+void solve() {
+	scanf("\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%c", &mat[i][j]);
+		}
+		scanf("\n");
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			
+			int sum = 0;
+
+			if (mat[i][j] == '*') ans[i][j] = '*';
+			else {
+				for (int k = 0; k < 8; k++) {
+					int nx = i + dir[k][0], ny = j + dir[k][1];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+					else if(mat[nx][ny] == '*') sum++;
+				}
+				ans[i][j] = sum + '0';
+			}
+		}
+	}
+
+	printf("Field #%d:\n", number++);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%c", ans[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+int main()
+{
+	while (scanf("%d %d", &n, &m) && m != 0 && n != 0) {
+		solve();
+	}
+	return 0;
+}
